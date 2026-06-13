54e09c64b0841451b4cfd808c945f0b88427fb45
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-06-13 16:04:27 +0330

    Add DaisyUI v4 setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
